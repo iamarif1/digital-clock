e6f97343435a0b2e481bb1d045ee2eff6a52d292
iamarif1 <iamarif121@gmail.com>
2021-02-21 11:49:48 +0600

    Digital Clock with Dynamic Progress Bar

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const hour = document.getElementById('hour');
-const minute = document.getElementById('minute');
-const second = document.getElementById('second');
-const progress = document.getElementById('progress');
+const hours = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+const progress = document.getElementById("progress");
 
 function showCurrentTime() {
-    let date = new Date();
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
+  let setDate = new Date();
+  let hr = setDate.getHours();
+  let min = setDate.getMinutes();
+  let sec = setDate.getSeconds();
 
-    hour.textContent = hr;
-    minute.textContent = min;
-    second.textContent = sec;
-
-    progress.style.width = (sec / 60) * 100 + '%'
+  hours.textContent = hr;
+  minute.textContent = min;
+  second.textContent = sec;
+  progress.style.width = (sec / 60) * 100 + '%';
 }
-
-setInterval(showCurrentTime, 1000)
+setInterval(showCurrentTime, 1000);
